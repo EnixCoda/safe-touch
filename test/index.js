fe596ac8b306a31d37b48de5194adb159ba66b56
EnixCoda <enixcoda@gmail.com>
2018-07-15 17:28:18 +0800

    test: update test cases for function

--- test/index.js
+++ test/index.js
@@ -2,6 +2,9 @@ const safeTouch = require('../lib').default
 
 const normalObject = {
   existProperty: null,
+  whatsThis() {
+    return this
+  },
 }
 const touched = safeTouch(normalObject)
 
@@ -29,9 +32,44 @@ log(
 log(
   'You can access non-exist property safely.\n',
   '(touched.go.deeper.even.random[Math.random()]() === undefined) is',
-  touched.go.deeper.even.random[Math.random()]() === undefined
+  touched.go.deeper.even.random[Math.random()]() === undefined,
 )
+
+log(
+  'What happens with functions?\n',
+  `normalObject.whatsThis = function () { return this }`,
+  'Access function property will NOT preserve `this`.\n',
+  'Does (touched.whatsThis()()) equal to normalObject.whatsThis()?',
+  touched.whatsThis()() === normalObject.whatsThis(),
+)
+
+log('---------------------------------------------------------------')
+
+~[
+  ['empty string', ''],
+  ['normal string', 'str'],
+  ['number 0', 0],
+  ['number 1', 1],
+  ['NaN', NaN],
+  ['Infinity', Infinity],
+  ['null', null],
+  ['undefined', undefined],
+  ['RegExp', /d/],
+  ['Array', []],
+  ['log function', log],
+].forEach(typeCheck)
+
+log('---------------------------------------------------------------')
 
 function log(...args) {
   console.log(...args, '\n')
+}
+
+function typeCheck([name, v]) {
+  const vv = safeTouch(v)
+  const evaluated = vv()
+  const vf = `${evaluated}` === ''
+    ? JSON.stringify(evaluated)
+    : evaluated
+  log(vv[Math.random()]() === undefined && 'safely', 'accessed', `${name},`, 'got', `(${vf})`, '.')
 }
